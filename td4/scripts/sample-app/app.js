c4c8db57911f80904e7eb59819b2174c9d231666
Auguste Sagaert <auguste.sagaert@gmail.com>
2025-11-27 00:47:29 +0100

    Complete lab on Version Control, Build Systems, and Automated Testing

---
+++ td4/scripts/sample-app/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+app.get('/name/:name', (req, res) => {
+    res.send(`Hello, ${req.params.name}!`);
+});
+module.exports = app;
---
+++ td4/scripts/tofu/live/lambda-sample/main.tf
@@ -0,0 +1,8 @@
+variable "aws_region" {
+  default = "us-east-1"
+}
+
+# Mock output for testing purposes if we were to run this
+output "api_endpoint" {
+  value = "http://example.com" 
+}
